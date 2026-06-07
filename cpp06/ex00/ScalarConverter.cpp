b97d9c8095268c00cc414802cb7516e0be205325
Georgii Tretiakov <gtretiak@c2r3s4.42porto.com>
2026-06-07 13:58:21 +0100

    ex00 done

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -14,6 +14,13 @@ ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &An) {
 	return (*this);
 }
 
+static void	printInvalid() {
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
+}
+
 void	ScalarConverter::printChar(double num) {
 	std::cout << "char: ";
 	if (num != num || num < 0 || num > 127)
@@ -41,24 +48,32 @@ void	ScalarConverter::printInt(double num) {
 void	ScalarConverter::printFloat(double num) {
 	std::cout << "float: ";
 	if (num == static_cast<int>(num))
-		std::cout << static_cast<float>(num) << std::fixed << std::setprecision(1);
+		std::cout << std::fixed << std::setprecision(1) << static_cast<float>(num);
 	else
-		std::cout << static_cast<float>(num) << std::fixed;
-	std::cout << ".f" << std::endl;
+		std::cout << static_cast<float>(num);
+	std::cout << "f" << std::endl;
+	if (num == static_cast<int>(num))
+	{
+		std::cout.unsetf(std::ios::floatfield);
+		std::cout.precision(6);
+	}
 }
 void	ScalarConverter::printDouble(double num) {
 	std::cout << "double: ";
 	if (num == static_cast<int>(num))
-		std::cout << static_cast<double>(num) << std::fixed << std::setprecision(1) << std::endl;
+		std::cout << std::fixed << std::setprecision(1) << static_cast<double>(num) << std::endl;
 	else
-		std::cout << static_cast<double>(num) << std::fixed << std::endl;
+		std::cout << static_cast<double>(num) << std::endl;
+	if (num == static_cast<int>(num))
+	{
+		std::cout.unsetf(std::ios::floatfield);
+		std::cout.precision(6);
+	}
 }
 
 double	ScalarConverter::parse(ScalarConverter::Type_ type, const std::string &var) {
 	char	*end;
-	if (type == INVALID)
-		throw "Unexpected type";
-	else if (type == CHAR)
+	if (type == CHAR)
 	{
 		if (var.length() == 1)
 			return static_cast<double>(var[0]);
@@ -68,36 +83,59 @@ double	ScalarConverter::parse(ScalarConverter::Type_ type, const std::string &va
 	return (std::strtod(var.c_str(), &end));
 }
 
+static bool	rejectNonDecimal(const std::string &var) {
+	size_t	i = 0;
+	if (var[i] == '+' || var[i] == '-')
+		++i;
+	if (i >= var.size() || !std::isdigit(var[i]))
+		return true;
+	if (var.find('x') != std::string::npos || var.find('X') != std::string::npos)
+		return true;
+	if (var[i] == '0' && i + 1 < var.size())
+	{
+		if (var[i + 1] != '.')
+			return true; //042
+	}
+	return (false);
+}
+
 ScalarConverter::Type_	ScalarConverter::detectType(const std::string &var) {
 	if (var == "-inf" || var == "+inf" || var == "inf" || var == "nan")
 		return PSEUDO_D;
 	else if (var == "inff" || var == "-inff" || var == "+inff" || var == "nanf")
 		return PSEUDO_F;
-	else if ((var.length() == 1 && std::isdigit(var[0])) || (var.length() == 3 && var[0] == '\'' && var[2] == '\''))
+	else if ((var.length() == 1 && !std::isdigit(var[0])) || (var.length() == 3 && var[0] == '\'' && var[2] == '\''))
 		return CHAR;
+	if (rejectNonDecimal(var))
+		return INVALID;
 	char	*end;
 	double	num = std::strtod(var.c_str(), &end);
-	if (num >= std::numeric_limits<int>::min() && num <= std::numeric_limits<int>::max())
-		return INT;
-	else if (*end == 'f' && *(end + 1) == '\0')
+	if (*end == 'f' && std::isdigit(*(end - 1)) && *(end + 1) == '\0' && var.find('.') != std::string::npos)
 		return FLOAT;
 	else if (*end == '\0')
-		return DOUBLE;
+	{
+		if (var.find('.') != std::string::npos)
+			return DOUBLE;
+		else if (num >= std::numeric_limits<int>::min() && num <= std::numeric_limits<int>::max())
+			return INT;
+		else
+			return INVALID;
+	}
 	else
 		return INVALID;
 }
 
 void	ScalarConverter::convert(const std::string &var) {
 	ScalarConverter::Type_	type = detectType(var);
-	try {
-		double	val = parse(type, var);
-		printChar(val);
-		printInt(val);
-		printFloat(val);
-		printDouble(val);
+	if (type == INVALID)
+	{
+		printInvalid();
+		return ;
 	}
-	catch (std::exception &e) {
-		throw "Unexpected type";
-	}
+	double	val = parse(type, var);
+	printChar(val);
+	printInt(val);
+	printFloat(val);
+	printDouble(val);
 }
 ScalarConverter::~ScalarConverter() {}
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -8,13 +8,6 @@ int	main(int argc, char **argv) {
 		std::cerr << "Invalid number of arguments" << std::endl;
 		return 1;
 	}
-	try {
-		ScalarConverter::convert(argv[1]);
-		return 0;
-	}	
-	catch (std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
+	ScalarConverter::convert(argv[1]);
+	return 0;
 }
