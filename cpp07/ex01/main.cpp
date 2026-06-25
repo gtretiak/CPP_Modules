ca24d31c4cc50ee012b65894801b1d7de9acdee7
Georgii Tretiakov <george22021988@gmail.com>
2026-06-25 12:56:18 +0000

    comments added

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -4,9 +4,9 @@ int	main(void) {
 	int	x[] = {1, 2, 3, 4, 5};
 	float	f[] = {21.21f, 7.77f};
 	const float	cf[] = {42.42f, 3.14f};
-	::iter(x, sizeof(x) / sizeof(x[0]), ::doubleValue<int>);
+	::iter(x, sizeof(x) / sizeof(x[0]), ::doubleValue<int>); // instantiated function template
 	::iter(f, sizeof(f) / sizeof(f[0]), ::doubleValue);
-	::iter(x, sizeof(x) / sizeof(x[0]), ::printValue);
+	::iter(const_cast<const int *>(x), sizeof(x) / sizeof(x[0]), ::printValue);//with explicit casting to const
 	::iter(f, sizeof(f) / sizeof(f[0]), ::printValue<float>);
 	::iter(cf, sizeof(cf) / sizeof(cf[0]), ::printValue);
 }
