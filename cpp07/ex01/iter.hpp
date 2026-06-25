e8aa5bff56e16e4eb070933ed2f3c9dd0a40d649
Georgii Tretiakov <george22021988@gmail.com>
2026-06-25 12:45:07 +0000

    ex01 done

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,10 +1,29 @@
 #ifndef ITER_HPP
 #define ITER_HPP
+#include <iostream>
 
-template <typename T, template F>
-void	iter(T *addr, const size_t len, F f) {
+template <typename T>
+void	iter(T *addr, const size_t len, void (*f)(T &val)) {
 	for (size_t i = 0; i < len; i++)
 		f(addr[i]);
+}
+
+template <typename T>
+void	iter(const T *addr, const size_t len, void (*f)(const T &val)) {
+	for (size_t i = 0; i < len; i++)
+		f(addr[i]);
+}
+
+template <typename T>
+void	doubleValue(T &val) {
+	std::cout << "This value was " << val;
+	val *= 2;
+	std::cout << " It's doubled. New value is " << val << std::endl;
+}
+
+template <typename T>
+void	printValue(const T &val) {
+	std::cout << "Just printing this value: " << val << std::endl;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,8 +1,12 @@
 #include "iter.hpp"
-#include <iostream>
 
 int	main(void) {
-	int	x = 2;
-	float	f = 42.42f;
-	iter(x, doubleValue(
+	int	x[] = {1, 2, 3, 4, 5};
+	float	f[] = {21.21f, 7.77f};
+	const float	cf[] = {42.42f, 3.14f};
+	::iter(x, sizeof(x) / sizeof(x[0]), ::doubleValue<int>);
+	::iter(f, sizeof(f) / sizeof(f[0]), ::doubleValue);
+	::iter(x, sizeof(x) / sizeof(x[0]), ::printValue);
+	::iter(f, sizeof(f) / sizeof(f[0]), ::printValue<float>);
+	::iter(cf, sizeof(cf) / sizeof(cf[0]), ::printValue);
 }
