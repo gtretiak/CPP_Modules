62ede0ccd57b3145f73d88e26724488e87f8856d
Georgii Tretiakov <george22021988@gmail.com>
2026-08-03 14:25:47 +0000

    initial commit

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,5 +1,7 @@
 #include "BitcoinExchange.hpp"
 
-int	main(void) {
-	;
+int	main(int argc, char **argv) {
+	if (argc != 2)
+		std::cerr << "Invalid number of arguments. Database is expected" << std::endl;
+	(void)argv;
 }
