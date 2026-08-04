847d5374340c4fb643eb82d00f411f5b7bdbce39
Georgii Tretiakov <george22021988@gmail.com>
2026-08-04 08:01:26 +0000

    minor changes

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -4,10 +4,10 @@
 
 class	BitcoinExchange {
 	private:
-		std::map<std::string, float>	data_;
-		bool	isValidDate(std::string date) const;
-		bool	isValidValue(double value) const;
-		float	getExchangeRate(std::string date) const;
+		std::map<std::string, double>	data_;
+		bool	isValidDate(const std::string &date) const;
+		bool	isValidValue(const double &value) const;
+		double	getExchangeRate(const std::string &date) const;
 		DataBaseException {}; // TODO
 	public:
 		void	loadDB(std::string filename); // std::ifstream, std::getline, std::stringstream, atof, strtod
@@ -22,7 +22,7 @@ For each line:
 Validate date format (check for YYYY-MM-DD).
 Validate pipe |.
 Validate value (0 to 1000).
-Find rate in map.
+Find rate in map. with map.lower_bound(date) finds the element that is >= date (if doesn't match previos one)
 Multiply and print.*/
 
 #endif
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -3,5 +3,5 @@
 int	main(int argc, char **argv) {
 	if (argc != 2)
 		std::cerr << "Invalid number of arguments. Query in format \"date | value\" expected" << std::endl;
-	(void)argv;
+	(void)argv; //TODO
 }
