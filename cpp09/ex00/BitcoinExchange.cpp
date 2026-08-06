92094801fcd037d3a53cd0306e8c002b5c8bf37c
gtretiak <george22021988@gmail.com>
2026-08-06 23:19:32 +0100

    defining the structure

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,6 +1,7 @@
 #include "BitcoinExchange.hpp"
 #include <map>
 #include <string>
+#include <sstream>
 #include <iostream>
 
 // Orthodox Canonical Form:
@@ -8,7 +9,7 @@ BitcoinExchange::BitcoinExchange () : DBPath_("./DataBase/data.csv") {
 	this->data_ = loadDB(DBPath_);
 } 
 BitcoinExchange::BitcoinExchange(const std::string &DBPath) : DBPath_(DBPath) {
-	this->data_ = loadDB(DBPath_);
+	loadDB();
 }
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &An) : DBPath_(An.DBPath_), data_(An.data_) {}
 BitcoinExchange	&BitcoinExchange::operator=(const BitcoinExchange &An) {
@@ -27,63 +28,50 @@ void	BitcoinExchange::processQuery(const std::string &filename) {
 	if (!file.is_open())
 		throw std::runtime_error("Error: could not open input file.");
 	std::string	line;
-	std::getline(file, line);
-	verifyHeader(line, false);
+	std::getline(file, line); // to discard the first line
 	while (std::getline(file, line))
 	{
 		parseLine(line, false);
 		if (this->resDate_ || this->resValue_)
 		{
-			printErrors(this->resDate_, this->resValue_);
+			printErrors();
 			continue ;
 		}
-		double	res = getExchangeRate()// TODO
-		compute() //TODO
-		std::cout << line << std::endl; // printResult TODO
+		double	res = Exchange();
+		std::cout << this->currentDate_ << " => " << this->currentValue_ << " = " << res << std::endl;
 	}
 	file.close();
 }
 
 // Internal Functions (helpers):
-void	BitcoinExchange::loadDB(const std::string &filename) {
-	std::ifstream	file(filename.c_str());
+void	BitcoinExchange::loadDB() {
+	std::ifstream	file(this->DBPath_.c_str());
 	if (!file.is_open())
 		throw std::runtime_error("Error: could not open database.");
 	std::string	line;
-	std::getline(file, line);
-	verifyHeader(line, true);
+	std::getline(file, line); // to discard the first line
 	while (std::getline(file, line))
 	{
 		parseLine(line, true);
-		double		price;
-		//assign pair: this->data_[date] = price;
+		//verify each line to avoid later comparison with garbage
+		convertDate();
+		this->data_[this->currentDate_] = this->currentValue_;
 	}
 	file.close();
 }
-void	BitcoinExchange::verifyHeader(std::string &header, bool isDB) {
-	std::stringstream	ss(header);
-	std::string	first, second;
-	if (isDB)
-		std::getline(ss, first, ',');
-	else
-		std::getline(ss, first, '|');
-	std::getline(ss, second);
-	if (isDB && (first != "date" || second != "exchange_rate"))
-		throw std::runtime_error("Error: Data base malformatted.");
-	if (!isDB && (first != "date" || second != "value"))
-		throw std::runtime_error("Error: Input files malformatted.");
-}
 void	BitcoinExchange::parseLine(std::string	&line, bool isDB) {
 	std::stringstream	ss(line);
+	//verify each line of DB to avoid comparison with garbage; throw
+	//verify each line of FILE; print errors and continue
 	if (isDB)
+	{
 		std::getline(ss, this->currentDate_, ',');
+		if (this->currentDate_.empty())
+			throw std::runtime_error("Error: bad input => " + this->currentDate_);
+	}
 	else
 		std::getline(ss, this->currentDate_, '|');
 	std::getline(ss, this->currentValueStr_);
-	//throw if database corrupted (invalid format)
-	//validate line
-	//format date
-	//store value
 	if (!isDB)
 	{
 		this->resDate_ = isValidDate(this->currentDate_);
@@ -91,41 +79,58 @@ void	BitcoinExchange::parseLine(std::string	&line, bool isDB) {
 	}
 	//
 	char	*end;
-	double	value = std::strtod(valueStr.c_str(), &end);
+	this->currentValue_ = std::strtod(this->currentValueStr_.c_str(), &end);
 	if (*end != '\n' || *end != '\0')
+		std::cerr << "Error: bad input => " << this->currentValueStr_ << std::endl;
 	if (!isValidValue(value))
 	{
-		std::cerr << "Error: " << errorValue << std::endl;
+		std::cerr << "Error: " << e`rrorValue << std::endl;
 		continue ;
 	}
 }
-void	BitcoinExchange::printErrors(ValidationResult date, ValidationResult value) const {
-	if (line is malformatted)
+void	BitcoinExchange::convertDate() {
+	std::string	month, day, year;
+	std::stringstream	ss(this->currentDate_);
+	std::getline(ss, month, '/');
+	std::getline(ss, day, '/');
+	std::getline(ss, year);
+	std::ostringstream	out;
+	out << year << '-' << std::setw(2) << std::setfill('0') << month << '-' << std::setw(2) << std::setfill('0') << day;
+	this->currentDate_ = out.c_str();
+}
+void	BitcoinExchange::printErrors() const {
+	if (this->resDate_)
+		std::cerr << "Error: bad input => " << this->resDate_ << std::endl;
+	switch (this->resValue_)
 	{
-		std::cerr << "Error: " << errorFormat << what << std::endl;
-		continue ;
-	}
-	std::string	date;
-	if (!isValidDate(date))
-	{
-		std::cerr << "Error: " << errorDate << std::endl;
-		continue ;
+		case (0):
+			return ;
+		case (2):
+			std::cerr << "Error: not positive number." << std::endl;
+			break ;
+		case (3):
+			std::cerr << "Error: too large number." << std::endl;
+			break ;
+
 	}
 }
-ValidationResult	BitcoinExchange::isValidDate(const std::string &date) const {
+ValidationResult	BitcoinExchange::isValidDate() {
 	if (wrong)
+	// length = 10
+	// index 4 and 7 should be '-'
+	// rest are digits
+	// extracting to year, month, day
+	// range 1-12 for month, 1-31 for day (but not always), leap years
 		return 1;
 	return 0;
 }
 ValidationResult	BitcoinExchange::isValidValue(const double &value) const {
-	if (wrong type)
+	if (value < 0)
 		return 2;
-	else if (value < 0)
-		return 3;
 	else if (value > 1000)
-		return 4;
+		return 3;
 	return 0;
 }
-double	BitcoinExchange::getExchangeRate(const std::string &value) {
+double	BitcoinExchange::Exchange(const std::string &value) {
 	map.lower_bound()
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -7,19 +7,19 @@ class	BitcoinExchange {
 	private:
 		std::string	DBPath_;
 		std::map<std::string, double>	data_;
-		std::string	currDate_;
-		std::string	currValueStr_;
-		double		currValue_;
+		std::string	currentDate_;
+		std::string	currentValueStr_;
+		double		currentValue_;
 		ValidationResult	resDate_;
 		ValidationResult	resValue_;
 
-		void	loadDB(const std::string &filename);
-		void	verifyHeader(std::string &header, bool isDB);
+		void	loadDB();
 		void	parseLine(std::string &line, bool isDB);
-		void	printErrors(ValidationResult Date, ValidationResult value) const;
+		void	convertDate();
+		void	printErrors() const;
 		ValidationResult	isValidDate(const std::string &date) const;
 		ValidationResult	isValidValue(const double &value) const;
-		double	getExchangeRate(const std::string &value);
+		double	Exchange();
 	public:
 		BitcoinExchange(); // "./DataBase/data.csv" by default
 		BitcoinExchange(const std::string &DBPath);
@@ -33,19 +33,14 @@ class	BitcoinExchange {
 enum	ValidationResult {
 	VALID,
 	BAD_DATE,
-	BAD_TYPE,
 	NEGATIVE,
 	TOO_LARGE
 };
 
 /*
-Summary Checklist:
-Read data.csv, parse comma, store in map.
-Read input.txt.
 For each line:
 Validate date format (check for YYYY-MM-DD).
 Validate pipe |.
-Validate value (0 to 1000).
 Find rate in map. with map.lower_bound(date) finds the element that is >= date (if doesn't match previos one)
 Multiply and print.*/
 
